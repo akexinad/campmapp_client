375b62906fc7f852e4518da268f2d3556b3aca5d
danny <danixeka@gmail.com>
2019-03-03 14:13:22 +1100

    successfully changing the state in the Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import SERVER from '../utils.js';
 import Nav from './Nav.js';
+import GoogleMapReact from 'google-map-react';
+import Sites from './Sites.js';
 
 import '../App.css';
 
@@ -8,12 +10,22 @@ export default class Home extends Component {
   constructor() {
     super();
     this.state = {
+      campSites: {},
+      ids: [],
       names: [],
       locations: [],
       latitudes: [],
       longitudes: [],
     }
-    // XXX: I think I will need the state to be an array of latitudes and longitudes
+  }
+
+  /* google-map-react documentation suggests this syntax to pass down the default props to the Google Map component */
+  static defaultProps = {
+    center: {
+      lat: -33.663411,
+      lng: 150.017931
+    },
+    zoom: 9,
   }
 
   componentDidMount() {
@@ -21,39 +33,62 @@ export default class Home extends Component {
     .then( results => {
 
       const campSites = results.data;
+
+      let campSiteIds = [];
       let campSiteNames = [];
       let campSiteLocations = [];
       let campSiteLatitudes = [];
       let campSiteLongitudes = [];
 
       for (let i = 0; i < campSites.length; i++) {
+        campSiteIds.push(campSites[i].id);
         campSiteNames.push(campSites[i].name);
         campSiteLocations.push(campSites[i].location);
         campSiteLatitudes.push(campSites[i].latitude);
         campSiteLongitudes.push(campSites[i].longitude);
       }
-      // console.log(campSiteNames);
-      // console.log(campSiteLocations);
-      // console.log(campSiteLatitudes);
-      // console.log(campSiteLongitudes);
 
       this.setState({
+        campSites: campSites,
         names: campSiteNames,
         locations: campSiteLocations,
         latitudes: campSiteLatitudes,
         longitudes: campSiteLongitudes,
       })
+
+      console.log(this.state.campSites);
     })
     .catch( error => {
       console.error(error);
     })
   }
 
+  // componentWillUnmount() {
+  // }
+
+
   render() {
     return (
-      <div>
+      <div
+        className="home-page"
+        style={{
+          height: '100vh',
+          width: '100%',
+        }}
+      >
         <Nav />
-        <h2>Home.js Component</h2>
+        <h2>HOME COMPONENT</h2>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBhZ3J2771KPwnoP287p1_b640KMp7hJwY' }}
+          defaultCenter={ this.props.center }
+          defaultZoom={ this.props.zoom }
+        >
+          <Sites
+            lat={ -34.116484 }
+            lng={ 150.210383 }
+            text={ 'X' }
+          />
+        </GoogleMapReact>
       </div>
     );
   }
