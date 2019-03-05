70b6bbf84ecb3c55584697c1e8a969da7f3075d9
danny <danixeka@gmail.com>
2019-03-05 16:07:58 +1100

    removed commented api key

--- src/components/Home.js
+++ src/components/Home.js
@@ -142,7 +142,3 @@ export default class Home extends Component {
     );
   }
 }
-
-// bootstrapURLKeys={{ key: API.key }}
-
-// bootstrapURLKeys={{ key: 'AIzaSyBhZ3J2771KPwnoP287p1_b640KMp7hJwY' }}
